3859fc54d23c762b9979962810a2f387b8aa10eb
pepe132 <jose.garciadom4gmail.com>
2023-07-07 00:34:53 -0600

    tienda verano

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -10,7 +10,7 @@ export const Footer = () => {
 			<div className='box'>
 				<figure>
 				
-					<img src='https://res.cloudinary.com/dwvfkyc6l/image/upload/v1685829761/foto_papa_gci86g.jpg' alt='logo-byfeliz'/>
+					<img src='https://res.cloudinary.com/dwvfkyc6l/image/upload/v1688710940/logoverano_giezzf.jpg' alt='logo-byfeliz'/>
 					
 				</figure>
 			</div>
